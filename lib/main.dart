c83a7705f26469fcce267075c7ead4c93b00d531
GurinderpreetSingh <sgurinderpreet@gmail.com>
2025-08-27 15:54:24 -0700

    moved floatingactionbutton to main, swicthed expansionpanellist to expansiontiles

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    void _floatingAction() {}
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -51,6 +52,17 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text("Hello World"),
       ),
       body: Center(child: body[currIndex]),
+      // floatingActionButton: Container(
+      //   padding: EdgeInsets.fromLTRB(0, 0, 10, 10),
+      //   child: Align(
+      //     alignment: Alignment.centerRight,
+      //     child: FloatingActionButton(
+      //       onPressed: _floatingAction,
+      //       tooltip: 'Increment',
+      //       child: const Icon(Icons.add),
+      //     ),
+      //   ),
+      // ),
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) {
           setState(() {
--- lib/screens/listitems.dart
+++ lib/screens/listitems.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 class Listitems extends StatelessWidget {
   const Listitems({super.key});
 
-  void _floatingAction() {}
-
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -20,129 +18,64 @@ class Listitems extends StatelessWidget {
           ),
           Container(
             padding: EdgeInsets.all(20),
-            color: Colors.amber,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ExpansionPanelList(
+                ExpansionTile(
+                  expandedAlignment: Alignment.topLeft,
+                  title: Text("Past Due"),
                   children: [
-                    ExpansionPanel(
-                      headerBuilder: (context, isOpen) {
-                        return Text(
-                          "Past Due",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 15,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        );
-                      },
-                      body: Text("body"),
-                      isExpanded: true,
+                    Container(
+                      child: ListTile(
+                        title: Text("Hello World"),
+                        leading: Icon(Icons.check),
+                      ),
+                    ),
+                    Container(
+                      child: ListTile(
+                        title: Text("Hello World"),
+                        leading: Icon(Icons.error),
+                      ),
+                    ),
+                  ],
+                ),
+                ExpansionTile(
+                  expandedAlignment: Alignment.topLeft,
+                  title: Text("Today"),
+                  children: [
+                    Container(
+                      child: ListTile(
+                        title: Text("Hello World"),
+                        leading: Icon(Icons.check),
+                      ),
+                    ),
+                    Container(
+                      child: ListTile(
+                        title: Text("Hello World"),
+                        leading: Icon(Icons.error),
+                      ),
+                    ),
+                  ],
+                ),
+                ExpansionTile(
+                  expandedAlignment: Alignment.topLeft,
+                  title: Text("Future"),
+                  children: [
+                    Container(
+                      child: ListTile(
+                        title: Text("Hello World"),
+                        leading: Icon(Icons.check),
+                      ),
+                    ),
+                    Container(
+                      child: ListTile(
+                        title: Text("Hello World"),
+                        leading: Icon(Icons.error),
+                      ),
                     ),
                   ],
                 ),
               ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            color: Colors.blue,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Ongoing",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Divider(height: 10, thickness: 1, color: Colors.blueGrey),
-                Column(
-                  children: [
-                    ExpansionPanelList(
-                      children: [
-                        ExpansionPanel(
-                          headerBuilder: (context, isOpen) {
-                            return Text("Ongoing 1");
-                          },
-                          body: Text("Ongoing item 1"),
-                          isExpanded: true,
-                        ),
-                      ],
-                    ),
-                    ExpansionPanelList(
-                      children: [
-                        ExpansionPanel(
-                          headerBuilder: (context, isOpen) {
-                            return Text("Ongoing 2");
-                          },
-                          body: Text("Ongoing item 2"),
-                          isExpanded: false,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            color: Colors.deepOrange,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Ending Soon",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Divider(height: 10, thickness: 1, color: Colors.blueGrey),
-                Column(
-                  children: [
-                    ExpansionPanelList(
-                      children: [
-                        ExpansionPanel(
-                          headerBuilder: (context, isOpen) {
-                            return Text("Ending Soon 1");
-                          },
-                          body: Text("Ending Soon item 1"),
-                          isExpanded: false,
-                        ),
-                      ],
-                    ),
-                    ExpansionPanelList(
-                      children: [
-                        ExpansionPanel(
-                          headerBuilder: (context, isOpen) {
-                            return Text("Ending Soon 2");
-                          },
-                          body: Text("Ending Soon item 2"),
-                          isExpanded: false,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-
-          Container(
-            padding: EdgeInsets.fromLTRB(0, 0, 10, 10),
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: FloatingActionButton(
-                onPressed: _floatingAction,
-                tooltip: 'Increment',
-                child: const Icon(Icons.add),
-              ),
             ),
           ),
         ],
